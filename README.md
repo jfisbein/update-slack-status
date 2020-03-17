e0cfa04c052fddf8a3ae6a31146bfe78437ac63e
Joan Fisbein <joan.fisbein@clarity.ai>
2020-03-17 11:06:52 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# update-slack-status
+update-slack-status
+===================
+
 Bash script to update slack status based on network info
+
+Usage
+-----
+
+First you need to open it in your favorite editor and replace the configuration variables with your own values.
+
+### Launch manually
+Simply running `./update-slack-status.sh`
+
+### Launch on startup
+run `crontab -e` and at the end of the crontab file add a line like this:
+
+`@reboot ( sleep 30 ; sh /location/update-slack-status.sh )`
+
+### Launch on network connection
+```bash
+sudo cp update-slack-status.sh /etc/network/if-up.d
+sudo chown root:root /etc/network/if-up.d/update-slack-status.sh
+sudo chmod 755 /etc/network/if-up.d/update-slack-status.sh
+```
+
+Improvements
+------------
+Possible future improvements
+
+* Change status based on current country (using geoip?)
+* Change status for ethernet only connections (using mac address of gateway?)
---
+++ update-slack-status.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+### REPLACE ALL THIS VARS WITH YOUR OWN VALUES
+# how to get a token: https://api.slack.com/legacy/custom-integrations/legacy-tokens
+SLACK_TOKEN="xoxp-123456789-123456789-123456789-123456789123456789"
+
+declare -a AT_WORK_SSIDS=("MyOffice-Floor1" "MyOffice-Floor2" "MyOffice-Floor3")
+declare -a AT_HOME_SSIDS=("MyHouse" "MyHouse-5Ghz")
+
+AT_HOME_MESSAGE="Working remotely"
+AT_HOME_EMOJI=":house_with_garden:"
+
+AT_WORK_MESSAGE="Working from office"
+AT_WORK_EMOJI=":office:"
+
+WORK_END_TIME="19:00"
+### END CONFIGURATION VARS
+
+# Updates slack status
+# param 1: Slack status text
+# param 2: Slack status emoji
+# param 3: Slack status expiration
+function slack-update-status() {
+    local TEXT="${1}"
+    local EMOJI="${2}"
+    local EXPIRATION="${3}"
+
+    local DATA=$(printf '{"status_text":"%s","status_emoji":"%s","status_expiration":%s}' "${TEXT}" "${EMOJI}" "${EXPIRATION}")
+
+    curl --silent -X POST https://slack.com/api/users.profile.set \
+        --data-urlencode "profile=${DATA}" \
+        --data-urlencode "token=${SLACK_TOKEN}"
+}
+
+# Resets slack status
+function slack-reset-status() {
+    local DATA='{"status_text":"","status_emoji":""}'
+
+    curl --silent -X POST https://slack.com/api/users.profile.set \
+        --data-urlencode "profile=${DATA}" \
+        --data-urlencode "token=${SLACK_TOKEN}"
+}
+
+# Log message to syslog
+# param 1: Message to log
+function log() {
+    logger --tag "slack-status" "$*"
+}
+
+# Check if a String is contined in a list of strings
+# param 1: List of Strings space separated
+# param 2: String to check
+# returns: 0 if found, 1 otherwise.
+function array-contains() {
+    local ARRAY="${1}"
+    local NEEDLE="${2}"
+
+    echo "1: ${ARRAY}"
+    echo "2: ${NEEDLE}"
+    for ELEMENT in $ARRAY; do
+        if [[ "${ELEMENT}" = "${NEEDLE}" ]]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+# Main function
+function main() {
+    local SSID=$(iwgetid -r)
+    log "Connected to ${SSID}"
+    if array-contains "${AT_WORK_SSIDS[*]}" "${SSID}"; then
+        log "Seting WORK status"
+        slack-update-status "${AT_WORK_MESSAGE}" "${AT_WORK_EMOJI}" $(date -d "$WORK_END_TIME today" +"%s") > /dev/null
+    elif array-contains "${AT_HOME_SSIDS[*]}" "${SSID}"; then
+        log "Setting HOME status"
+        slack-update-status "${AT_HOME_MESSAGE}" "${AT_HOME_EMOJI}" $(date -d "$WORK_END_TIME today" +"%s") > /dev/null
+    else
+        log "Reseting status"
+        slack-reset-status
+    fi
+}
+
+main
